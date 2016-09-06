2dbeb4952116050c68882c73309aab10b15b751f
pongy20 <eric.dupont@hotmail.de>
2016-09-06 17:20:18 +0200

    w

--- Plot/src/de/plots/generation/PlotGeneration.java
+++ Plot/src/de/plots/generation/PlotGeneration.java
@@ -8,6 +8,7 @@ public class PlotGeneration {
 
 	/**
 	 * Notice that the min and max parameter is the totaly outside of the plot!
+	 * Notice that widht is the amiunt of Block within the way and border (normally 37)
 	 * 
 	 * @author Eric D.
 	 */
@@ -16,9 +17,9 @@ public class PlotGeneration {
 		
 		//Removing every single Block at the Plot
 		
-		for (int x = min.getBlockX(); x < max.getBlockX(); x++) {
-			for (int z = min.getBlockZ(); z < max.getBlockZ(); z++) {
-				for (int y = min.getBlockY(); y <= max.getBlockY(); y++ ) {
+		for (int x = min.getBlockX(); x <= max.getBlockX(); x++) {
+			for (int z = min.getBlockZ(); z <= max.getBlockZ(); z++) {
+				for (int y = 0; y <= 256; y++ ) {
 					Location temp = new Location(world, x, y, z);
 					world.getBlockAt(temp).setType(Material.AIR);
 				}
@@ -27,6 +28,19 @@ public class PlotGeneration {
 		
 		//generating the plot without border & way
 		
+		for (int x = min.getBlockX() + way; x < max.getBlockX() - way; x++) {
+			for (int z = min.getBlockZ() + way; z < max.getBlockZ() - way; z++) {
+				for (int y = 2; y < stoneheight; y++ ) {
+					//Generating the stone blocks
+					Location stone = new Location(world, x, y, z);
+					world.getBlockAt(stone).setType(Material.STONE);
+					
+					//setting the Bedrock
+					Location bedrock = new Location(world, x, 1, z);
+					world.getBlockAt(bedrock).setType(Material.BEDROCK);
+				}
+			}
+		}
 	}
 	
 }
