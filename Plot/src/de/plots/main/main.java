3b1161c221d8d72d80cd4c1af0b7ab4b35c96182
pongy20 <eric.dupont@hotmail.de>
2016-09-20 19:30:00 +0200

    mysql Anbindung

--- Plot/src/de/plots/main/main.java
+++ Plot/src/de/plots/main/main.java
@@ -4,23 +4,33 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.plots.commands.createCommand;
+import de.plots.commands.PlotCommand;
+import de.plots.mysql.mysql;
 
 public class main extends JavaPlugin {
  
+	public static int waysize = 3;
+	public static int plotsize = 32;
+	
 	public void onEnable() {
 		registerEvents();
 		registerCommands();
+		
+		mysql.createTable();
+		mysql.connect();
 		
 		System.out.println("[Plots] wurde gestartet!");
 	}
 	
 	public void onDisable() {
+		
+		mysql.close();
+		
 		System.out.println("[Plots] wurde deaktiviert!");
 	}
 	private void registerCommands() {
 		
-		this.getCommand("create").setExecutor(new createCommand());
+		this.getCommand("plot").setExecutor(new PlotCommand());
 	}
 	private void registerEvents() {
 		PluginManager pm = Bukkit.getServer().getPluginManager();
---
+++ Plot/src/de/plots/mysql/mysql.java
@@ -0,0 +1,54 @@
+package de.plots.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+
+public class mysql {
+	
+	public static String host = "miniecraft.de";
+	public static String port = "3306";
+	public static String database = "Plotserver";
+	public static String username = "root";
+	public static String password = "maverick98";
+	public static Connection con;
+	
+	public static void connect() {
+		if (!isConnected()) {
+			try {
+				con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
+				Bukkit.getConsoleSender().sendMessage("§c[MYSQL Database] wurde verbunden!");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} 
+	}
+	public static void close() {
+		if (isConnected()) {
+			try {
+				con.close();
+				Bukkit.getConsoleSender().sendMessage("§c[MYSQL Database] wurde getrennt!");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static boolean isConnected() {
+		return (con == null ? false : true);
+	}
+	public static Connection getConnection() {
+		return con;
+	}
+	public static void createTable() {
+		String s = "CREATE TABLE IF NOT EXISTS Plots (ID INT(100), PlayernameVARCHAR(100), World VARCHAR(100), UUID VARCHAR(100), start_x VARCHAR(100), start_y VARCHAR(100), end_x VARCHAR(100), end_y VARCHAR(100))";
+		try {
+			PreparedStatement ps = mysql.getConnection().prepareStatement(s);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
